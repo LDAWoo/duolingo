1d09a5578fcafb2505f87e5287465c27d0ba8afa
LDAWoo <leducanhvu3006@gmail.com>
2025-02-26 15:29:07 +0700

    Update page.tsx

--- app/(main)/(routes)/lesson/page.tsx
+++ app/(main)/(routes)/lesson/page.tsx
@@ -1,6 +1,6 @@
 import { getLesson, getUserProgress } from "@/db/queries";
 import React from "react";
-import Quiz from "./quiz";
+// import Quiz from "./quiz";
 import { redirect } from "next/navigation";
 
 const LessonPage = async () => {
@@ -15,7 +15,11 @@ const LessonPage = async () => {
 
     const initialPercentage = (data.lesson.challenges.filter((challenge) => challenge.completed).length / data.lesson.challenges.length) * 100;
 
-    return <Quiz initialLessonId={data.lesson.lessonId} initialLessonChallenges={data.lesson.challenges} initialHearts={userProgress.hearts} initialPercentage={initialPercentage} userSubscription={null} />;
+    return (
+        // <Quiz initialLessonId={data.lesson.lessonId} initialLessonChallenges={data.lesson.challenges} initialHearts={userProgress.hearts} initialPercentage={initialPercentage} userSubscription={null} />
+
+        <div>Quiz</div>
+    );
 };
 
 export default LessonPage;
