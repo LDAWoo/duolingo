4978659f0bdbd62f390b7950c83b9c08336c0a47
LDAWoo <leducanhvu3006@gmail.com>
2025-02-26 15:31:27 +0700

    Update page.tsx

--- app/(main)/(routes)/lesson/page.tsx
+++ app/(main)/(routes)/lesson/page.tsx
@@ -1,25 +1,31 @@
-import { getLesson, getUserProgress } from "@/db/queries";
-import React from "react";
-// import Quiz from "./quiz";
-import { redirect } from "next/navigation";
+// import { getLesson, getUserProgress } from "@/db/queries";
+// import React from "react";
+// // import Quiz from "./quiz";
+// import { redirect } from "next/navigation";
+
+// const LessonPage = async () => {
+//     const lessonData = getLesson();
+//     const userProgressData = getUserProgress();
+
+//     const [data, userProgress] = await Promise.all([lessonData, userProgressData]);
+
+//     if (!data || !data.lesson || !data.lesson.lessonId || !userProgress) {
+//         return redirect("/learn");
+//     }
+
+//     const initialPercentage = (data.lesson.challenges.filter((challenge) => challenge.completed).length / data.lesson.challenges.length) * 100;
+
+//     return (
+//         // <Quiz initialLessonId={data.lesson.lessonId} initialLessonChallenges={data.lesson.challenges} initialHearts={userProgress.hearts} initialPercentage={initialPercentage} userSubscription={null} />
+
+//         <div>Quiz</div>
+//     );
+// };
+
+// export default LessonPage;
 
 const LessonPage = async () => {
-    const lessonData = getLesson();
-    const userProgressData = getUserProgress();
-
-    const [data, userProgress] = await Promise.all([lessonData, userProgressData]);
-
-    if (!data || !data.lesson || !data.lesson.lessonId || !userProgress) {
-        return redirect("/learn");
-    }
-
-    const initialPercentage = (data.lesson.challenges.filter((challenge) => challenge.completed).length / data.lesson.challenges.length) * 100;
-
-    return (
-        // <Quiz initialLessonId={data.lesson.lessonId} initialLessonChallenges={data.lesson.challenges} initialHearts={userProgress.hearts} initialPercentage={initialPercentage} userSubscription={null} />
-
-        <div>Quiz</div>
-    );
+    return <div>Quiz</div>;
 };
 
 export default LessonPage;
